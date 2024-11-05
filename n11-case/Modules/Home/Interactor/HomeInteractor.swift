1edc2fe4a8b3f3a1f1562cb9e203f54569848f81
simgeatlhn <simgeeatlihan@gmail.com>
2024-11-05 16:58:05 +0300

    added HomeInteractor

---
+++ n11-case/Modules/Home/Interactor/HomeInteractor.swift
@@ -0,0 +1,19 @@
+//
+//  HomeInteractor.swift
+//  n11-case
+//
+//  Created by simge on 5.11.2024.
+//
+
+import Foundation
+
+class HomeInteractor {
+    func fetchProducts() {
+        
+    }
+    
+    
+    func fetchSponsoredProducts() {
+        
+    }
+}
