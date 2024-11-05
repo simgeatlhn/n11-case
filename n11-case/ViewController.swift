28aa74d7631b741a91d30b6256ad435f12f9079c
simgeatlhn <simgeeatlihan@gmail.com>
2024-11-05 13:57:09 +0300

    first commit

---
+++ n11-case/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  n11-case
+//
+//  Created by simge on 5.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
