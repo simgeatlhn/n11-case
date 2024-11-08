82d7e20b5020dcfcae7cbf7fb362cc4381598bb9
simgeatlhn <simgeeatlihan@gmail.com>
2024-11-08 13:28:55 +0300

    updated  NetworkManager to use error extension

--- n11-case/Modules/NetworkManager/NetworkManager.swift
+++ n11-case/Modules/NetworkManager/NetworkManager.swift
@@ -23,22 +23,29 @@ class NetworkManager {
     
     func fetchData<T: Decodable>(from url: URL, completion: @escaping (Result<T, Error>) -> Void) {
         AF.request(url).validate().responseDecodable(of: T.self) { response in
-            switch response.result {
-            case .success(let data):
-                completion(.success(data))
-            case .failure(let error):
-                if let statusCode = response.response?.statusCode {
-                    completion(.failure(NetworkError.serverError(statusCode: statusCode)))
-                } else if (error as NSError).code == NSURLErrorNotConnectedToInternet {
-                    completion(.failure(NetworkError.noData))
-                } else if (error as NSError).code == NSURLErrorCannotDecodeContentData {
-                    completion(.failure(NetworkError.decodingError))
-                } else {
-                    completion(.failure(NetworkError.unknownError))
+                switch response.result {
+                case .success(let data):
+                    completion(.success(data))
+                case .failure(let error):
+                    let networkError = error.toNetworkError(statusCode: response.response?.statusCode)
+                    completion(.failure(networkError))
                 }
             }
-        }
     }
 }
 
-
+extension Error {
+    func toNetworkError(statusCode: Int? = nil) -> NetworkError {
+        if let code = statusCode, !(200...299).contains(code) {
+            return .serverError(statusCode: code)
+        }
+        switch (self as NSError).code {
+        case NSURLErrorNotConnectedToInternet:
+            return .noData
+        case NSURLErrorCannotDecodeContentData:
+            return .decodingError
+        default:
+            return .unknownError
+        }
+    }
+}
